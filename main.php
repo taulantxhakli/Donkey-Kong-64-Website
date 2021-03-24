bb05be527bdc9251367dbc88639aa9a1203b49b3
unknown <txhakli17@stac.edu>
2021-03-24 19:03:55 -0400

    re-pushing the website
    changed the file extension from html to php.

---
+++ main.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>DK 64 Wiki</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="design.css">
+    <link rel="stylesheet" href="gal.css">
+
+</head>
+<body>
+
+<div class="header">
+  <h1>Donkey Kong 64</h1>
+  <h2>Meet the crew</h2>
+</div>
+
+<div class="navbar">
+  <a href="main.html" class="active">Characters</a>
+  <a href="maps.html">Levels</a>
+  <a href="secrets.html">Secrets</a>
+  <a href="gallery.html">Gallery</a>
+</div>
+
+<div class="row">
+  <div class="side">
+    <h2>Playable Characters</h2>
+    <h4>Donkey Kong:</h4>
+    <img src="assets/dk.jpg" style="height:200px;">
+    <p>Leader of the bunch. Donkey Kong is a well-rounded ape with the basics for the player to get started. He collects yellow bananas, coins, and blueprints
+      (as shown below). 
+    </p>
+    <img src="assets/yellow_Banana.gif" style="height:90px;"><img src="assets/yellow_Banana_Coin.gif" style="height:90px;"><img src="assets/yellow_Blueprint.gif" style="height:90px;">
+    <h4>Diddy Kong:</h4>
+    <img src="assets/diddy.jpg" style="height:200px;">
+    <p>Twin peanut shootering monkey. Diddy Kong is the side-kick to the great ape Donkey. Diddy collects red collectables (as shown below).</p>
+    <img src="assets/red_Banana.gif" style="height:90px;"><img src="assets/red_Banana_Coin.gif" style="height:90px;"><img src="assets/red_Blueprint.gif" style="height:90px;">
+    <h4>Tiny Kong:</h4>
+    <img src="assets/tiny.jpg" style="height:200px;">
+    <p>Feather shooting chimp. Tiny Kong is the best traversal character out of the bunch. She collects all purple collectables (as shown below).</p>
+    <img src="assets/purple_Banana.gif" style="height:90px;"><img src="assets/purple_Banana_Coin.gif" style="height:90px;"><img src="assets/purple_Blueprint.gif" style="height:90px;">
+    <h4>Chunky Kong:</h4>
+    <img src="assets/chunky.jpg" style="height:200px;">
+    <p>Big gorilla, with an even bigger heart. Chunky Kong is the strong-arm gorilla that can punch through fences and
+      pick of large bolders. He collects all green collectables (as shown below).</p>
+    <img src="assets/green_Banana.gif" style="height:90px;"><img src="assets/green_Banana_Coin.gif" style="height:90px;"><img src="assets/green_Blueprint.gif" style="height:90px;">
+    <h4>Lanky Kong:</h4>
+    <img src="assets/lanky.jpg" style="height:200px;">
+    <p>Grape peashooter with long arms for distance fighting and handstand walking. Lanky Kong is the ranged fighter and silly orangutan of the team.
+      He collects all the blue collectables (as shown below).
+    </p>
+    <img src="assets/blue_Banana.gif" style="height:90px;"><img src="assets/blue_Banana_Coin.gif" style="height:90px;"><img src="assets/blue_Blueprint.gif" style="height:90px;">
+
+  </div>
+  <div class="main">
+    <h2>Bosses</h2>
+    <h4>King Krusha K. Rool</h4>
+    <img src="assets/king.jpg" style="height:500px;">
+    <p>The King of the Kremlings. Donkey Kong's mortal enemy. This fight will test the player like never before. All Kongs will have a turn in battering the king of Kremlings
+      in order to stop him. Be warned, King K. Rool does not play fair, as his Kremling minions save him from the bell every knock-down.
+    </p>
+    <img src="assets/donkeykong.gif" style="height:300px;"><img src="assets/kingkkrool.gif" style="height:300px;">
+    <p></p>
+    <br>
+    <h4>Army Dillo</h4>
+    <img src="assets/armadillo.jpg" style="height:500px;">
+    <p>Army Dillo is the first boss that Donkey Kong faces against in the Jungle Japes level. He has two stages, one is him with the two cannons, with the other not having cannons. DK needs to use the explosive barrel in order to defeat him.
+      In the second fight, the Kremlings will upgrade the boss to try and defeat DK. This time, the Army Dillo will have jetpack launchers (as shown on the left) where his rocket launchers are to hover over the ground and slam down to deliver a devastating blow.
+      Once the armadillo is finished, he will replace the jetpack with the rocket launchers after a roll back to the center (as shown on the right) to fire at DK once again.
+    </p>
+    <img src="assets/armydillorockets.gif" style="height:300px;"><img src="assets/rocket.gif" style="height:300px;">
+    <br>
+    <h4>Dogadon</h4>
+    <img src="assets/fly.jpg" style="height:500px;">
+    <p>Mother to the baby Dogadon, which was squashed by Diddy Kong, she is the second boss encounter in the Angry Aztec level and is challenged by Diddy himself. Though, this will not be the only time the 
+      player will face this beast. In the Fungi Forest level, Chunky Kong finds himself stuck in the same lava pit to face off against Dogadon once more.
+    </p>
+    <br>
+    <h4>Mad Jack</h4>
+    <img src="assets/madjack.jpg" style="height:500px;">
+    <p>The killer-joke in the box, Tiny Kong challenges Mad Jack to a platform-based fight in the third boss in the Frantic Factory level.</p>
+    <br>
+    <h4>Puftoss</h4>
+    <img src="assets/puftoss.jpg" style="height:500px;">
+    <p>In the pirate level of Gloomy Galleon, Lanky Kong finds himself trapped in the water with Puftoss.</p>
+    <br>
+    <h4>King Kut Out</h4>
+    <img src="assets/kkutout.jpg" style="height:500px;">
+    <p>As the Kongs go through the Creepy Castle level, they are met with the boss himself...King Kut Out? The Kremlings create a cut out of King K. Rool to scare the Kongs, but they do not know that the Kongs see right through the cardboard.
+      To defeat this art project, the Kongs need to wait for the Kut Out to appear for them to launch themselves through the cannons and break off the pieces of cardboard from Kut Out (as shown on the left).
+      But, the Kongs need to tred carefully, as the Kut Out will shoot lasers out of its eyes while Kremlings dressed as ghosts will distract the Kongs (as shown on the right).
+    </p>
+    <img src="assets/brokearm.gif" style="height:300px;"><img src="assets/enemykrem.gif" style="height:300px;">
+    <br>
+  </div>
+</div>
+
+<div class="siteFooterBar">
+    <div class="content">
+        <img src="assets/footer_symbol.png" >
+            <div class="foot">2021 @</div>
+            <div class="foot">Taulant Xhakli</div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ secrets.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>DK 64 Wiki</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="design.css">
+
+</head>
+<body>
+
+<div class="header">
+  <h1>Donkey Kong 64</h1>
+  <h2>Secret Rooms & Collectibles</h2>
+</div>
+
+<div class="navbar">
+  <a href="main.html">Characters</a>
+  <a href="maps.html">Levels</a>
+  <a href="secrets.html" class="active">Secrets</a>
+  <a href="gallery.html">Gallery</a>
+</div>
+
+<div class="row">
+  <div class="side">
+    <h2>About Me</h2>
+    <h5>Photo of me:</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+    <h3>More Text</h3>
+    <p>Lorem ipsum dolor sit ame.</p>
+    <div class="fakeimg" style="height:60px;">Image</div><br>
+    <div class="fakeimg" style="height:60px;">Image</div><br>
+    <div class="fakeimg" style="height:60px;">Image</div>
+  </div>
+  <div class="main">
+    <h2>TITLE HEADING</h2>
+    <h5>Title description, Dec 7, 2017</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text..</p>
+    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    <br>
+    <h2>TITLE HEADING</h2>
+    <h5>Title description, Sep 2, 2017</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text..</p>
+    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+  </div>
+</div>
+
+<div class="siteFooterBar">
+    <div class="content">
+        <img src="assets/footer_symbol.png" >
+            <div class="foot">2021 @</div>
+            <div class="foot">Taulant Xhakli</div>
+    </div>
+</div>
+
+</body>
+</html>
